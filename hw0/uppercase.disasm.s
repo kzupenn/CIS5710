cce6c3d0e62605e08fdd966fcb1903d9173df3fe
kzupenn <kz2025@sas.upenn.edu>
2024-01-29 14:08:50 +0000

    fixes

--- hw0/uppercase.disasm.s
+++ hw0/uppercase.disasm.s
@@ -10,13 +10,19 @@ Disassembly of section .text:
 
 0001007c <convert_loop>:
    1007c:	00054283          	lbu	t0,0(a0)
-   10080:	00028a63          	beqz	t0,10094 <end_program>
-   10084:	fe028293          	addi	t0,t0,-32
-   10088:	00550023          	sb	t0,0(a0)
+   10080:	02028663          	beqz	t0,100ac <end_program>
+   10084:	06100313          	li	t1,97
+   10088:	07a00393          	li	t2,122
+   1008c:	0062be33          	sltu	t3,t0,t1
+   10090:	0053beb3          	sltu	t4,t2,t0
+   10094:	000e1863          	bnez	t3,100a4 <not_lowercase>
+   10098:	000e9663          	bnez	t4,100a4 <not_lowercase>
+   1009c:	fe028293          	addi	t0,t0,-32
+   100a0:	00550023          	sb	t0,0(a0)
 
-0001008c <not_lowercase>:
-   1008c:	00150513          	addi	a0,a0,1
-   10090:	fedff06f          	j	1007c <convert_loop>
+000100a4 <not_lowercase>:
+   100a4:	00150513          	addi	a0,a0,1
+   100a8:	fd5ff06f          	j	1007c <convert_loop>
 
-00010094 <end_program>:
-   10094:	0000006f          	j	10094 <end_program>
+000100ac <end_program>:
+   100ac:	0000006f          	j	100ac <end_program>
