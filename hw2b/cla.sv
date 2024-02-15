2ffd1a3fcba8c67f9e955f076860c4697fb31edf
kzupenn <kz2025@sas.upenn.edu>
2024-02-15 18:20:23 +0000

    gp4

--- hw2b/cla.sv
+++ hw2b/cla.sv
@@ -1,4 +1,8 @@
 `timescale 1ns / 1ps
+/*
+Kevin Zhang, kz2025
+Justina Lam, jklam 
+*/
 
 /**
  * @param a first 1-bit input
@@ -27,6 +31,11 @@ module gp4(input wire [3:0] gin, pin,
            output wire [2:0] cout);
 
    // TODO: your code here
+   assign gout = |gin;
+   assign pout = &pin;
+   assign cout[0]= gin[0] | (pin[0]&cin);
+   assign cout[1]= gin[1] | (pin[1]&gin[0]) | pin[1]&pin[0]&cin;
+   assign cout[2]= gin[2] | (pin[2]&gin[1]) | pin[2]&pin[1]&gin[0] | pin[2]&pin[1]&pin[0]&cin;
 
 endmodule
 
@@ -37,6 +46,10 @@ module gp8(input wire [7:0] gin, pin,
            output wire [6:0] cout);
 
    // TODO: your code here
+   assign gout = |gin;
+   assign pout = &pin;
+   gp4 u1(.gin(gin[3:0]), .pin(pin[3:0]), .cin(1'b0), .gout(), .pout(), .cout(cout[2:0]));
+   gp4 u2(.gin(gin[7:4]), .pin(pin[7:4]), .cin(cout[2]), .gout(), .pout(), .cout(cout[6:4]));
 
 endmodule
 
@@ -45,6 +58,14 @@ module cla
    input wire         cin,
    output wire [31:0] sum);
 
+   wire [31:0] p, g;
+   p = a^b;
+   g = a&b;
    // TODO: your code here
+   gp8 u1(.gin(), .pin(), .cin(), .gout(), .pout(), .cout());
+   gp8 u2(.gin(), .pin(), .cin(), .gout(), .pout(), .cout());
+   gp8 u3(.gin(), .pin(), .cin(), .gout(), .pout(), .cout());
+   gp8 u4(.gin(), .pin(), .cin(), .gout(), .pout(), .cout());
+
 
 endmodule
